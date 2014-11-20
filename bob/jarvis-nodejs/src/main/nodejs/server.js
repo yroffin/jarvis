9a6800d1d165006f4774ff5257d59628427f33c3
yannick <yannick@yannick-HP>
2014-11-20 01:08:01 +0100

    Version initiale

--- bob/jarvis-nodejs/src/main/nodejs/server.js
+++ bob/jarvis-nodejs/src/main/nodejs/server.js
@@ -1,10 +1,31 @@
-var express = require('express');
-var https = require('https');
-var http = require('http');
-var fs = require('fs');
-var blammo = require('blammo');
+/* 
+ * Copyright 2014 Yannick Roffin.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 function main() {
+	// Middleware
+	var express = require('express');
+	var https = require('https');
+	var http = require('http');
+	var fs = require('fs');
+	var blammo = require('blammo');
+
+	// App part
+	var routes = require(__dirname + '/routes/routes');
+	var servicesConfig = require(__dirname + '/services/config');
+
 	var root = blammo.LoggerFactory.getLogger(blammo.Logger.ROOT_LOGGER_NAME);
 	var logger = blammo.LoggerFactory.getLogger('logger1');
 
@@ -16,6 +37,19 @@ function main() {
 
 	// Create a service (the app object is just a callback).
 	var app = express();
+	
+	// Activate cookies, sessions and forms
+	app.use(express.logger())
+	.use(express.static('public'))
+	.use(express.favicon(__dirname + '/public/favicon.ico'))
+	.use(express.cookieParser())
+	.use(express.session({secret: 'secretkey'}))
+	.use(express.bodyParser());
+
+	/**
+	 * build all routes
+	 */
+	routes.init(app);
 
 	// Create an HTTP service.
 	logger.info('Create an HTTP service');
