1c49859e9c95208c0a0f7d536f74e1c4f4a615c3
yroffin@gmail.com <yroffin@gmail.com>
2014-11-30 00:19:39 +0100

    Update

--- jarvis-core/jarvis-nodejs/src/main/start.cmd
+++ jarvis-core/jarvis-nodejs/src/main/start.cmd
@@ -1,5 +1,5 @@
-SET NODE_HOME=W:\git-luna\bob\jarvis-nodejs\node
-SET WORKSPACE=W:\git-luna\bob\jarvis-nodejs\src\main
+SET NODE_HOME=W:\git-luna\jarvis-core\jarvis-nodejs\node
+SET WORKSPACE=W:\git-luna\jarvis-core\jarvis-nodejs\src\main
 
 SET NODE_PATH=%WORKSPACE%\nodejs\node_modules\npm\node_modules;%WORKSPACE%\nodejs\node_modules\npm
 
