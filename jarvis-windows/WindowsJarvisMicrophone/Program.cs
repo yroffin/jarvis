9a16939241aca51374c6a23fb9076edbfb39fba5
yroffin@gmail.com <yroffin@gmail.com>
2014-11-30 22:34:27 +0100

    Passage .net 4.0

--- jarvis-windows/WindowsJarvisMicrophone/Program.cs
+++ jarvis-windows/WindowsJarvisMicrophone/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace WindowsJarvisClient
 {
@@ -14,9 +15,16 @@ namespace WindowsJarvisClient
         [STAThread]
         static void Main()
         {
+            //Win32.alert();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainJarvisClientForm());
         }
+    }
+
+    public class Win32
+    {
+        [DllImport("pocketsphinx.dll")]
+        public static extern int alert();
     }
 }
