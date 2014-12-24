696b5265b62eddb5bdde150c531e99bea59ef6f1
yroffin@gmail.com <yroffin@gmail.com>
2014-12-24 01:16:23 +0100

    Setup init

---
+++ jarvis-core/jarvis-nodejs/src/main/jarvis.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+#
+# Perform jarvis operation
+#
+# Note : this script must run on any platform
+# - linux
+# - windows (shell.w32-ix86 based Cf. http://sourceforge.net/projects/win-bash)
+#
+
+# Description : find node executable
+# Arguments: None
+# Returns: None
+cleanup()
+{
+	return 0
+}
+
+# Description : find node executable
+# Arguments: None
+# Returns: None
+findNodeExecutable()
+{
+	set -x
+	# first find it locally (in design mode)
+	for executable in `cd "${SCRIPT_WORKSPACE}" && find ./node -type f -name 'node.exe' 2>/dev/null`
+	do
+		echo ${SCRIPT_WORKSPACE}/`dirname $executable`
+		return
+	done
+	# find it in subdirs
+	for executable in `cd "${SCRIPT_WORKSPACE}" && find ../node -type f -name 'node.exe' 2>/dev/null`
+	do
+		echo ${SCRIPT_WORKSPACE}/`dirname $executable`
+		return
+	done
+	# find it in subdirs
+	for executable in `cd "${SCRIPT_WORKSPACE}" && find ../../node -type f -name 'node.exe' 2>/dev/null`
+	do
+		echo ${SCRIPT_WORKSPACE}/`dirname $executable`
+		return
+	done
+}
+
+# Description : find node executable
+# Arguments: None
+# Returns: None
+findServerJs()
+{
+	# first find it locally (in design mode)
+	for executable in `cd "${SCRIPT_WORKSPACE}" && find ./nodejs -type f -name 'server.js' 2>/dev/null`
+	do
+		echo ${SCRIPT_WORKSPACE}/`dirname $executable`
+		return
+	done
+	# find it in subdirs
+	for executable in `cd "${SCRIPT_WORKSPACE}" && find ../nodejs -type f -name 'server.js' 2>/dev/null`
+	do
+		echo ${SCRIPT_WORKSPACE}/`dirname $executable`
+		return
+	done
+}
+
+echo CWD: `pwd`
+export SCRIPT_WORKSPACE="`pwd`"
+
+export NODE_HOME="`findNodeExecutable`"
+export NODEJS_HOME="`findServerJs`"
+export NODE_PATH="${NODEJS_HOME}"/node_modules/npm/node_modules:"${NODEJS_HOME}"/node_modules/npm
+
+set | grep SCRIPT_WORKSPACE
+set | grep NODE
+
+export JARVIS_LOGS="${TEMP}"
+mkdir -p "${JARVIS_LOGS}"
+
+cd "${NODEJS_HOME}"
+"${NODE_HOME}/node.exe" "${NODEJS_HOME}/server.js"
+exit $?
--- jarvis-core/jarvis-nodejs/src/main/start.cmd
+++
@@ -1,7 +0,0 @@
-SET NODE_HOME=W:\git-luna\jarvis-core\jarvis-nodejs\node
-SET WORKSPACE=W:\git-luna\jarvis-core\jarvis-nodejs\src\main
-
-SET NODE_PATH=%WORKSPACE%\nodejs\node_modules\npm\node_modules;%WORKSPACE%\nodejs\node_modules\npm
-
-cd /d "%WORKSPACE%\nodejs"
-"%NODE_HOME%\node.exe" "%WORKSPACE%\nodejs\server.js"
