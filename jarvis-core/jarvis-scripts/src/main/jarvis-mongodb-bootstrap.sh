a28738a04d1bf3b67ce8643f44332ece312cdd24
yroffin@gmail.com <yroffin@gmail.com>
2015-06-01 00:00:48 +0200

    unix

--- jarvis-core/jarvis-scripts/src/main/jarvis-mongodb-bootstrap.sh
+++ jarvis-core/jarvis-scripts/src/main/jarvis-mongodb-bootstrap.sh
@@ -1,5 +1,4 @@
-@echo off
 export SCRIPT_WORKSPACE=`pwd`/
-cd "%SCRIPT_WORKSPACE%"
-echo ** Running script ${SCRIPT_WORKSPACE}jarvis-mongodb.sh %1 %2 %3 %4 %5 %6 %7 %8 %9
-bash "${SCRIPT_WORKSPACE}jarvis-mongodb.sh" %1 %2 %3 %4 %5 %6 %7 %8 %9
+cd "${SCRIPT_WORKSPACE}"
+echo ** Running script ${SCRIPT_WORKSPACE}jarvis-mongodb.sh $*
+bash "${SCRIPT_WORKSPACE}jarvis-mongodb.sh" $*
