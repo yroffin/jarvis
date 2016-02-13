cd80356831253ee54ea7ae50be42224689da6cb2
kazoar <kazoar@kazoar-PC.home>
2016-02-13 23:44:51 +0100

    Correction

--- jarvis-core/jarvis-core-neo4j-client/src/test/java/org/jarvis/neo4j/client/Neo4jTest.java
+++ jarvis-core/jarvis-core-neo4j-client/src/test/java/org/jarvis/neo4j/client/Neo4jTest.java
@@ -11,31 +11,10 @@ public class Neo4jTest {
 
 	@Test
 	public void test() {
-		CypherRestClient cypherRestClient = new CypherRestClient("http://localhost:7474","neo4j","123456");
-		Result res = cypherRestClient.execute("MATCH (n) RETURN n LIMIT 100");
-		System.err.println("Result:"+res);
-		int i = 0;
 	}
 
 	@Test
 	public void testCypherFindLink() {
-		String leftLabel = "BlockBean";
-		String rightLabel = "ScriptPluginBean";
-		String relId = "116";
-		
-		CypherRestClient cypherRestClient = new CypherRestClient("http://localhost:7474","neo4j","123456");
-		Result result = cypherRestClient.execute("MATCH (left:"+leftLabel+")-[r]->(right:"+rightLabel+") WHERE id(r) = "+relId+" RETURN id(r),r");
-
-		if(result.hasNext()) {
-			Map<String, Object> rows = result.next();
-			/**
-			 * set relationship properties
-			 */
-			String id = (String) (rows.get("id(r)")+"");
-			Node r = (Node) rows.get("r");
-			int i = 0;
-		}
-		int i = 0;
 	}
 
 }
