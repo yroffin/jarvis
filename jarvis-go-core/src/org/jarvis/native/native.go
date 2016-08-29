3e2efdf12242c813682f335fb1aba4ce3310ecea
kazoar <kazoar@jarvis.acme.com>
2016-08-29 23:33:05 +0200

    Test

--- jarvis-go-core/src/org/jarvis/native/native.go
+++ jarvis-go-core/src/org/jarvis/native/native.go
@@ -3,7 +3,7 @@ package syscall
 // #ifdef WIRINGPI
 // #cgo amd64 386 CFLAGS: -DMOCK
 // #else
-// #cgo LDFLAGS:
+// #cgo arm LDFLAGS:
 // void  delay             	(unsigned int howLong) {}
 // void  delayMicroseconds 	(unsigned int howLong) {}
 // unsigned int millis      (void) {}
@@ -13,20 +13,10 @@ package syscall
 // void pinMode             (int pin, int mode) {}
 // int  setuid      		(int uid) {return 0;}
 // #endif
-// #include "native.h"
-//import "C"
-
-import (
-	"syscall"
-)
-
-func Setuid(uid int) (err error) {
-	return syscall.EOPNOTSUPP
-}
+import "C"
 
 func InitWiringPi() int {
-	Setuid(0)
-	return 0
+	return int(C.wiringPiSetup())
 }
 
 func DioOn(pin int, sender int, interruptor int) int {
