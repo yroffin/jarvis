10484403a476c05ab6b83d4fc45edde18dd60788
kazoar <kazoar@jarvis.acme.com>
2016-08-30 23:33:18 +0200

    Recompilation suite modif connectors

--- jarvis-core/jarvis-core-ui/src/main/resources/public/ui/script.js
+++ jarvis-core/jarvis-core-ui/src/main/resources/public/ui/script.js
@@ -49,7 +49,6 @@ angular.module('JarvisApp', [
      'JarvisApp.services.cron',
      'JarvisApp.services.view',
      'JarvisApp.services.property',
-     'JarvisApp.services.connector',
      'JarvisApp.services.device',
      'JarvisApp.services.event',
      'JarvisApp.services.block',
@@ -63,6 +62,7 @@ angular.module('JarvisApp', [
      'jarvis.directives.trigger',
      'jarvis.directives.configuration',
      'jarvis.directives.notification',
+     'jarvis.directives.connector',
      /**
       * controllers
       */
@@ -73,7 +73,6 @@ angular.module('JarvisApp', [
      'JarvisApp.ctrl.crons',
      'JarvisApp.ctrl.views',
      'JarvisApp.ctrl.properties',
-     'JarvisApp.ctrl.connectors',
      'JarvisApp.ctrl.scenarios',
      'JarvisApp.ctrl.blocks',
      'JarvisApp.ctrl.home'
@@ -1239,35 +1238,6 @@ angular.module('JarvisApp.services.device', [])
 
 /* Services */
 
-/**
- * connectorResourceService
- */
-angular.module('JarvisApp.services.connector', []).factory('connectorResourceService', 
-		[ 'genericResourceService', function(genericResourceService) {
-  return {
-	  connector : genericResourceService.crud(['connectors'])
-  }
-}]);
-/* 
- * Copyright 2014 Yannick Roffin.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-'use strict';
-
-/* Services */
-
 angular.module('JarvisApp.services.event', [])
 	.factory('eventResourceService', [ 'genericResourceService', function(genericResourceService) {
 		return {
@@ -2297,12 +2267,12 @@ angular.module('JarvisApp.routes',['JarvisApp.config'])
         .state('connectors', {
             url: '/connectors',
             controller: 'connectorsCtrl',
-            templateUrl: '/ui/js/partials/connectors/page.html'
+            template: '<jarvis-widget-connectors></jarvis-widget-connectors>'
         })
         .state('connectors-by-id', {
             url: '/connectors/:id',
             controller: 'connectorCtrl',
-            templateUrl: '/ui/js/partials/connectors/connector/page.html'
+            template: '<jarvis-widget-connector></jarvis-widget-connector>'
         })
         .state('snapshots', {
             url: '/snapshots',
@@ -2668,24 +2638,6 @@ angular.module('JarvisApp.directives.widgets', ['JarvisApp.services'])
     templateUrl: '/ui/js/partials/properties/property/jarvis-property-general.html',
     link: function(scope, element, attrs) {
     	$log.debug('jarvis-property');
-    }
-  }
-}])
-.directive('jarvisConnectors', [ '$log', '$stateParams', function ($log, $stateParams) {
-  return {
-    restrict: 'E',
-    templateUrl: '/ui/js/partials/connectors/jarvis-connectors.html',
-    link: function(scope, element, attrs) {
-    	$log.debug('jarvis-connectors');
-    }
-  }
-}])
-.directive('jarvisConnectorGeneral', [ '$log', '$stateParams', function ($log, $stateParams) {
-  return {
-    restrict: 'E',
-    templateUrl: '/ui/js/partials/connectors/connector/jarvis-connector-general.html',
-    link: function(scope, element, attrs) {
-    	$log.debug('jarvis-connector-general');
     }
   }
 }])
@@ -3700,6 +3652,14 @@ angular.module('jarvis.directives.trigger', ['JarvisApp.services'])
 			$stateParams.id
 	);
     /**
+     * execute this trigger
+     */
+    $scope.execute = function(trigger) {
+    	triggerResourceService.trigger.task(trigger.id, 'execute',  {}, function(data) {
+   	    	toastService.info('trigger ' + trigger.name + '#' + trigger.id + ' executed');
+	    }, toastService.failure);
+    }
+    /**
      * loading
      */
     $scope.load = function() {
@@ -3863,6 +3823,131 @@ angular.module('jarvis.directives.notification', ['JarvisApp.services'])
   return {
     restrict: 'E',
     templateUrl: '/ui/js/directives/notification/partials/jarvis-notification-test.html',
+    link: function(scope, element, attrs) {
+    }
+  }
+}]);
+/* 
+ * Copyright 2014 Yannick Roffin.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+/* Ctrls */
+
+angular.module('jarvis.directives.connector', ['JarvisApp.services'])
+.controller('connectorsCtrl', 
+		['$scope', '$log', 'genericScopeService', 'jarvisWidgetConnectorService',
+	function($scope, $log, genericScopeService, connectorResourceService){
+	/**
+	 * declare generic scope resource (and inject it in scope)
+	 */
+	genericScopeService.scope.resources(
+			function(entities) {
+				$scope.connectors = entities;
+			},
+			function() {
+				return $scope.connectors;
+			},
+			$scope, 
+			'connectors', 
+			connectorResourceService.connector,
+			{
+    			name: "connector name",
+    			icon: "settings_input_antenna",
+    			isRenderer: false,
+    			isSensor: false,
+    			canAnswer: false
+    		}
+	);
+}])
+.controller('connectorCtrl',
+		['$scope', '$log', '$stateParams', '$mdDialog', 'genericResourceService', 'genericScopeService', 'jarvisWidgetConnectorService', 'toastService',
+	function($scope, $log, $stateParams, $mdDialog, genericResourceService, genericScopeService, connectorResourceService, toastService){
+	/**
+	 * declare generic scope resource (and inject it in scope)
+	 */
+	genericScopeService.scope.resource(
+			$scope, 
+			'connector', 
+			'connectors', 
+			connectorResourceService.connector);
+    /**
+     * verify this connector
+     */
+    $scope.ping = function(connector) {
+    	connectorResourceService.connector.task(connector.id, 'ping', connector, function(data) {
+    		/**
+    		 * check data for ping result
+    		 */
+    		$scope.status = angular.toJson(data, true);
+    	}, toastService.failure);
+    }
+    /**
+     * load this controller
+     */
+    $scope.load = function() {
+		/**
+		 * get current connector
+		 */
+    	genericResourceService.scope.entity.get($stateParams.id, function(update) {
+    		$scope.connector=update;
+    	}, connectorResourceService.connector);
+
+    	$log.info('connector-ctrl', $scope.connectors);
+    }
+}])
+.factory('jarvisWidgetConnectorService', [ 'genericResourceService', function( genericResourceService) {
+	return {
+		connector: genericResourceService.crud(['connectors'])
+	}
+}])
+/**
+ * commands
+ */
+.directive('jarvisWidgetConnectors', [ '$log', '$stateParams', function ($log, $stateParams) {
+  return {
+    restrict: 'E',
+    templateUrl: '/ui/js/directives/connector/jarvis-widget-connectors.html',
+    link: function(scope, element, attrs) {
+    }
+  }
+}])
+.directive('jarvisConnectors', [ '$log', '$stateParams', function ($log, $stateParams) {
+  return {
+    restrict: 'E',
+    templateUrl: '/ui/js/directives/connector/partials/jarvis-connectors.html',
+    link: function(scope, element, attrs) {
+    }
+  }
+}])
+/**
+ * command
+ */
+.directive('jarvisWidgetConnector', [ '$log', '$stateParams', function ($log, $stateParams) {
+  return {
+    restrict: 'E',
+    templateUrl: '/ui/js/directives/connector/jarvis-widget-connector.html',
+    link: function(scope, element, attrs) {
+    }
+  }
+}])
+.directive('jarvisConnectorGeneral', [ '$log', '$stateParams', function ($log, $stateParams) {
+  return {
+    restrict: 'E',
+    templateUrl: '/ui/js/directives/connector/partials/jarvis-connector-general.html',
     link: function(scope, element, attrs) {
     }
   }
@@ -4535,88 +4620,6 @@ angular.module('JarvisApp.ctrl.views', ['JarvisApp.services'])
     	genericResourceService.scope.collections.findAll('devices', $stateParams.id, $scope.devices, viewResourceService.devices);
 
 		$log.info('view-ctrl', $scope.views);
-    }
-}]);
-/* 
- * Copyright 2014 Yannick Roffin.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-'use strict';
-
-/* Ctrls */
-
-angular.module('JarvisApp.ctrl.connectors', ['JarvisApp.services'])
-.controller('connectorsCtrl', 
-		['$scope', '$log', 'genericScopeService', 'connectorResourceService',
-	function($scope, $log, genericScopeService, connectorResourceService){
-	/**
-	 * declare generic scope resource (and inject it in scope)
-	 */
-	genericScopeService.scope.resources(
-			function(entities) {
-				$scope.connectors = entities;
-			},
-			function() {
-				return $scope.connectors;
-			},
-			$scope, 
-			'connectors', 
-			connectorResourceService.connector,
-			{
-    			name: "connector name",
-    			icon: "settings_input_antenna",
-    			isRenderer: false,
-    			isSensor: false,
-    			canAnswer: false
-    		}
-	);
-}])
-.controller('connectorCtrl',
-		['$scope', '$log', '$stateParams', '$mdDialog', 'genericResourceService', 'genericScopeService', 'connectorResourceService', 'toastService',
-	function($scope, $log, $stateParams, $mdDialog, genericResourceService, genericScopeService, connectorResourceService, toastService){
-	/**
-	 * declare generic scope resource (and inject it in scope)
-	 */
-	genericScopeService.scope.resource(
-			$scope, 
-			'connector', 
-			'connectors', 
-			connectorResourceService.connector);
-    /**
-     * verify this connector
-     */
-    $scope.ping = function(connector) {
-    	connectorResourceService.connector.task(connector.id, 'ping', connector, function(data) {
-    		/**
-    		 * check data for ping result
-    		 */
-    		$scope.status = angular.toJson(data, true);
-    	});
-    }
-    /**
-     * load this controller
-     */
-    $scope.load = function() {
-		/**
-		 * get current connector
-		 */
-    	genericResourceService.scope.entity.get($stateParams.id, function(update) {
-    		$scope.connector=update;
-    	}, connectorResourceService.connector);
-
-    	$log.info('connector-ctrl', $scope.connectors);
     }
 }]);
 /* 
