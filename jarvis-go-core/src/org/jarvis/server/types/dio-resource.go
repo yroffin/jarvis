7f2b5c776036739ac9a9c223ba586aa09bd44724
Yannick Roffin <yroffin@gmail.com>
2016-09-11 22:41:42 +0200

    Externalisation GO

--- jarvis-go-core/src/org/jarvis/server/types/dio-resource.go
+++
@@ -1,9 +0,0 @@
-package types
-
-// DioResource : dio resource struct
-type DioResource struct {
-	Pin        int    `json:"pin"`
-	Sender     int    `json:"sender"`
-	Interuptor int    `json:"interruptor"`
-	On         string `json:"on"`
-}
