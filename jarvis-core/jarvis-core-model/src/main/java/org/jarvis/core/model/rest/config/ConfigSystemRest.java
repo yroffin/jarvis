f6acf98470516ce06664ed9c4110b75ca273fcdd
Yannick Roffin <yroffin@gmail.com>
2016-10-31 02:48:33 +0100

    Correction

---
+++ jarvis-core/jarvis-core-model/src/main/java/org/jarvis/core/model/rest/config/ConfigSystemRest.java
@@ -0,0 +1,53 @@
+/**
+ *  Copyright 2015 Yannick Roffin
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+package org.jarvis.core.model.rest.config;
+
+import java.util.List;
+
+import org.jarvis.core.model.rest.tools.CronRest;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * system configuration
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ConfigSystemRest {
+	/**
+	 * cron
+	 */
+	@JsonProperty("cron")
+	public boolean cron;
+
+	/**
+	 * scheduled
+	 */
+	@JsonProperty("scheduled")
+	public List<CronRest> scheduled;
+
+	/**
+	 * events, is inner thread active ?
+	 */
+	@JsonProperty("events")
+	public boolean events;
+	
+	@Override
+	public String toString() {
+		return "ConfigSystemRest [cron=" + cron + ", scheduled=" + scheduled + ", events=" + events + "]";
+	}
+}
