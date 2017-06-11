1c631d31480b579e38a3728007cba523a92713f3
Yannick Roffin <yroffin@gmail.com>
2017-06-12 01:17:58 +0200

    correction

--- jarvis-core/jarvis-core-ui-ng/src/app/model/device-bean.ts
+++ jarvis-core/jarvis-core-ui-ng/src/app/model/device-bean.ts
@@ -4,6 +4,7 @@ import { PluginScriptBean } from './plugin-bean';
 
 export class DeviceBean extends ResourceBean {
     public template: string;
+    public tagColor: string;
     public render: any;
     public triggers: TriggerBean[];
     public devices: DeviceBean[];
