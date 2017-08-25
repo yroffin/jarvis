7ff18c4a2360190001658efdde34d54f52114db9
Yannick Roffin <yroffin@gmail.com>
2017-08-25 22:06:03 +0200

    x

--- jenkins/include/build-jenkinsfile.groovy
+++ jenkins/include/build-jenkinsfile.groovy
@@ -1,27 +1,47 @@
 /**
- * prepare tools
+ * configure tools
  */
-def prepare() {
+def configure() {
+      /**
+       * define ip target
+       */
       env.MASTER_NODE = '192.168.1.111';
       env.SLAVE_NODE  = '192.168.1.73';
       env.SHIRKA_NODE  = '192.168.1.38';
       env.PIZERO_NODE = '192.168.1.47';
+}
 
-      stage('node') {
+/**
+ * prepare tools
+ */
+def prepare() {
+      stage('Prepare NodeJS Suite') {
             // NodeJS
             tool name: 'NodeJS', type: 'com.cloudbees.jenkins.plugins.customtools.CustomTool'
             env.PATH='/usr/bin:/bin:/var/jenkins_home/tools/com.cloudbees.jenkins.plugins.customtools.CustomTool/NodeJS/node-v6.10.0-linux-x64/bin'
       }
 
-      stage('maven') {
+      stage('Prepare maven tools') {
             // Maven
             tool name: 'M3', type: 'maven'
             mvnHome = tool 'M3'
       }
 
-      stage('git') {
+      stage('Create logs') {
             sh '''
                   mkdir -p ${WORKSPACE}/logs
+            '''
+      }
+
+      stage('@angular/cli') {
+            // NodeJS
+            tool name: 'angular-cli', type: 'com.cloudbees.jenkins.plugins.customtools.CustomTool'
+            sh '''
+                  node --version
+                  npm --version
+                  npm config get cache
+                  npm install -g @angular/cli@latest
+                  ng --version
             '''
       }
 }
